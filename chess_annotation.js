cfe3c6b773e540affea9542070862cd0fc5ad18e
Shao Hang He <shaohanghe91@gmail.com>
2013-08-25 12:49:32 -0400

    Added styles for different board sizes

--- chess_annotation.js
+++ chess_annotation.js
@@ -6,15 +6,14 @@
  */
 jQuery(document).ready(function()
 {
-   // initialize variables
+    // initialize variables
     var chess = "";
-
     //clear prev games
-     jQuery('#game-container').html('');
-  
+    jQuery('#game-container').html('');
     if (jQuery('#curr_game').html().length > 10)
     {
-       chess = new PgnViewer( 
+        var pieceSize = jQuery('#chess_annotation_pieceSize_setting').html();
+        chess = new PgnViewer(
                 {
                     boardName: "game",
                     //pgnFile: 'upper.pgn',  
@@ -26,13 +25,36 @@ jQuery(document).ready(function()
                     //showBracketsOnVariation: false,
                     hideBracketsOnTopLevelVariation: true,
                     moveAnimationLength: 0.1,
-                    pieceSize: jQuery('#chess_annotation_pieceSize_setting').html()
+                    pieceSize: pieceSize
                 }
         );
-          
+        //if it is not the default size, then change the style
+        if (pieceSize != 46)
+        {
+            jQuery('#chess_content').removeClass();
+            jQuery('#chessgame').removeClass();
+            jQuery('#chessgame_header').removeClass();
+            jQuery('#chessgame .movelist').addClass('movelist' + pieceSize);
+            jQuery('#chessgame .movelist').removeClass('movelist');
+            jQuery('#chessgame .board').addClass('board' + pieceSize);
+            jQuery('#chessgame .board').removeClass('board');
+
+            //re-adding style
+            jQuery('#chess_content').addClass('chess_content' + pieceSize);
+            jQuery('#chessgame').addClass('chessgame' + pieceSize);
+            jQuery('#chessgame_header').addClass('chessgame_header' + pieceSize);          
+        }
+        else
+        {
+            jQuery('#chess_content').removeClass();
+            jQuery('#chessgame').removeClass();
+            jQuery('#chessgame_header').removeClass();
+            jQuery('#chess_content').addClass('chess_content');
+            jQuery('#chessgame').addClass('chessgame');
+            jQuery('#chessgame_header').addClass('chessgame_header');
+        }
     }
-    
-     
+
     jQuery("#edit-chess-annotation-parsepgn").click(function() {
 
         var pgn_txt = jQuery("#edit-chess-annotation-pastepgn").val();
@@ -45,32 +67,28 @@ jQuery(document).ready(function()
             var content = parsePGNtoHTML(pgn_txt);
             content += '<div id = "chess_annotation_pieceSet_setting"  style = "display:none;" >' + pieceSet + '</div>';
             content += '<div id = "chess_annotation_pieceSize_setting"  style = "display:none;" >' + pieceSize + '</div>';
-             //the filter must be set to full html in order for the module to work
+            //the filter must be set to full html in order for the module to work
             jQuery('.filter-list').val('full_html');
             jQuery('#edit-chess-annotation-pgnview').html('');
             jQuery('#edit-chess-annotation-pgnview').html(content);
             return false;
         }
         /* //to be implemented
-        else if (png_upload.length > 1) {
-            jQuery.ajax({
-                url: Drupal.settings.basePath + 'chess_annotation/processPGN'
-            });
-        }*/
+         else if (png_upload.length > 1) {
+         jQuery.ajax({
+         url: Drupal.settings.basePath + 'chess_annotation/processPGN'
+         });
+         }*/
         return false;
     });
-    
-
 });
-
 function parsePGNtoHTML(pgn)
 {
 
     var prefix = jQuery("#chess_annotation_template").html();
     prefix = rebuildString(prefix, prefix.lastIndexOf('</pre>'));
-
     var suffix = "</pre>";
-    return '<div id ="chess_content">' + prefix + pgn + suffix + '</div>';
+    return '<div id ="chess_content" class = "chess_content">' + prefix + pgn + suffix + '</div>';
 }
 
 //need to be re-implemented!
@@ -81,14 +99,12 @@ function getPGNheader(pgn)
     var game_header = game_info.split("]");
     game_header.pop();
     var header_string = game_header.join("<br />");
-
     return header_string;
 }
 
 //utility function 
 function sanitizeString(val) {
     var result = '';
-
     for (var i = 0; i < val.length; i++) {
         var s = val[i];
         if (val.charCodeAt(i) > 31 && val.charCodeAt(i) < 127)
@@ -101,7 +117,6 @@ function sanitizeString(val) {
 //utility function 
 function rebuildString(val, index) {
     var result = '';
-
     for (var i = 0; i < val.length; i++) {
         if (i === index)
             break;
